20baef5419ef0e5b2397adb300035dbe097aed98
unknown <danniqueeg@gmail.com>
2021-12-19 17:43:03 +0100

    made navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import Navbar from './components/navbar/Navbar'
+import Topbar from './components/navbar/Navbar'
 import About from  './components/about/About'
 import Intro from './components/intro/Intro'
 import Portfolio from './components/portfolio/Portfolio'
 import Contact from './components/contact/Contact'
 import './app.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 //import {} from '@mui/icons-material';
-
 
 function App() {
   return (
     <div className="app">
 
-    <Navbar/>
+      <Topbar/>
       
-    <div className="sections">
-      <Intro/>
-      <About/>
-      <Portfolio/>
-      <Contact/>
-    </div>
-
-
-
-
+      <div className="sections">
+        <Intro/>
+        <About/>
+        <Portfolio/>
+        <Contact/>
+      </div>
 
     </div>
   );
