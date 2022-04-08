6fde488b7481f7fdddc58d96e4bac2f529c3175d
unknown <danniqueeg@gmail.com>
2022-04-08 14:01:59 +0200

    added favicon

--- server/server.js
+++ server/server.js
@@ -5,15 +5,18 @@ require('dotenv').config();
 const port = process.env.PORT || 5000; 
 
 
-app.listen(port, () => console.log(`Listening on port ${port}`)); 
 
-//app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get('/', (req, res) => { 
-
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   res.send("Data is send!"); 
 }); 
 
+
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-//   });
+//   })
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
